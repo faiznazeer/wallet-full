676523a4a937c59872957292c2f458ab9d74302d
Faiz Nazeer <faiznazeer777@gmail.com>
2024-05-07 00:10:30 +0530

    Added prisma schema

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,10 @@
+import { useBalance } from "@repo/store/hooks";
+
 export default function Page(): JSX.Element {
+  const balance = useBalance();
   return (
     <div className="bg-slate-400">
-      hi everyone!
+      hi everyone!{balance}
     </div>
   );
 }
